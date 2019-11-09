c40b92b33d3d0deedcc65a97a692ceef43edcc50
Salih <salihsert18@gmail.com>
2019-11-09 18:24:11 +0100

    add client side

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const config = require('config');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
+const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -146,7 +147,8 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
